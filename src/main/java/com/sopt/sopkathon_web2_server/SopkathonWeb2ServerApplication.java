55931a209fa26db5787c83cc80164e58c45d781d
kimhoeyun <clnvrd@suwon.ac.kr>
2026-05-14 19:00:42 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/sopt/sopkathon_web2_server/SopkathonWeb2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.sopt.sopkathon_web2_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopkathonWeb2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopkathonWeb2ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sopt/sopkathon_web2_server/SopkathonWeb2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.sopkathon_web2_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopkathonWeb2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
