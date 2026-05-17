e6242e612dd4272ccf4d2600327117c944617ee6
kimhoeyun <clnvrd@suwon.ac.kr>
2026-05-17 09:38:56 +0900

    chore: CORS 설정 추가

--- src/main/java/com/sopt/sopkathon_web2_server/global/config/WebConfig.java
+++ src/main/java/com/sopt/sopkathon_web2_server/global/config/WebConfig.java
@@ -10,9 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
                 .allowedHeaders("*")
+                .allowCredentials(true)
                 .maxAge(3000);
     }
 }
